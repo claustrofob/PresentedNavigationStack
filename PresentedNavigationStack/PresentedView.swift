75ec55f6a75fac0974ae814bdc5f313f1414e5ff
Mikalai Zmachynski <nikolai.zmachinsky@dev.gem4me.com>
2024-09-17 10:43:58 +0200

    init

--- PresentedNavigationStack/PresentedView.swift
+++ PresentedNavigationStack/PresentedView.swift
@@ -4,17 +4,28 @@ struct PresentedView: View {
     @Environment(\.dismiss) var dismiss
     @Environment(\.close) var close
     
+    @State var isPushed = false
+    
     let title: String
     
     var body: some View {
-        Text(title)
-            .navigationTitle(title)
-            .toolbar {
-                Button(title) {
-                    dismiss()
-                    close()
-                }
-                .tint(.pink)
+        VStack {
+            Text(title)
+            Button("Push") {
+                isPushed = true
             }
+        }
+        .navigationTitle(title)
+        .navigationBarTitleDisplayMode(.large)
+        .toolbar {
+            Button(title) {
+                dismiss()
+                close()
+            }
+            .tint(.pink)
+        }
+        .navigationDestination(isPresented: $isPushed) {
+            Text("Pushed view")
+        }
     }
 }
